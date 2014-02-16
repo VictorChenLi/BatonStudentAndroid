203fd9a8b79ad5bafa9283fc34e36255cb6c06d2
foxwendy <zhaoyi.lab@gmail.com>
2014-02-16 14:02:52 -0500

    can register to GCM, send register id to sync server and reveive plain message from sync server

---
+++ src/ca/utoronto/ece1778/baton/gcm/client/main/RegisterActivity.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.utoronto.ece1778.baton.gcm.client.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import ca.utoronto.ece1778.baton.util.AlertDialogManager;
+import ca.utoronto.ece1778.baton.util.CommonUtilities;
+
+/**
+ * Main UI for the demo app.
+ */
+public class RegisterActivity extends Activity {
+    // alert dialog manager
+    AlertDialogManager alert = new AlertDialogManager();
+     
+    // Internet detector
+    ConnectionDetector cd;
+     
+    // UI elements
+    EditText txtName;
+    EditText txtEmail;
+     
+    // Register button
+    Button btnRegister;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+         
+        cd = new ConnectionDetector(getApplicationContext());
+ 
+        // Check if Internet present
+        if (!cd.isConnectingToInternet()) {
+            // Internet Connection is not present
+            alert.showAlertDialog(RegisterActivity.this,
+                    "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+ 
+        // Check if GCM configuration is set
+        if (CommonUtilities.SERVER_URL == null || CommonUtilities.SENDER_ID == null || CommonUtilities.SERVER_URL.length() == 0
+                || CommonUtilities.SENDER_ID.length() == 0) {
+            // GCM sernder id / server url is missing
+            alert.showAlertDialog(RegisterActivity.this, "Configuration Error!",
+                    "Please set your Server URL and GCM Sender ID", false);
+            // stop executing code by return
+             return;
+        }
+         
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+         
+        /*
+         * Click event on Register button
+         * */
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+             
+            @Override
+            public void onClick(View arg0) {
+                // Read EditText dat
+                String name = txtName.getText().toString();
+                String email = txtEmail.getText().toString();
+                 
+                // Check if user filled the form
+                if(name.trim().length() > 0 && email.trim().length() > 0){
+                    // Launch Main Activity
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                     
+                    // Registering user on our server                   
+                    // Sending registraiton details to MainActivity
+                    i.putExtra("name", name);
+                    i.putExtra("email", email);
+                    startActivity(i);
+                    finish();
+                }else{
+                    // user doen't filled that data
+                    // ask him to fill the form
+                    alert.showAlertDialog(RegisterActivity.this, "Registration Error!", "Please enter your details", false);
+                }
+            }
+        });
+    }
+ 
+}
